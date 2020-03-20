1a2aa48921537003b5b6aca6a6e2b330453c557e
Milo Wissig <mwissig@gmail.com>
2020-03-20 17:19:10 -0400

    pagination bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,18 @@
 class PagesController < ApplicationController
   def home
-    @users = User.all.order("name ASC").paginate(:page => params[:page], :per_page => 30)
+    @users = User.all.order("name ASC").paginate(:page => params[:page], :per_page => 20)
   end
 
   def asc
-        @users = User.includes(:status).order("statuses.rating ASC").paginate(:page => params[:page], :per_page => 30)
+        @users = User.includes(:status).order("statuses.rating ASC").paginate(:page => params[:page], :per_page => 20)
   end
 
   def desc
-        @users = User.includes(:status).order("statuses.rating DESC").paginate(:page => params[:page], :per_page => 30)
+        @users = User.includes(:status).order("statuses.rating DESC").paginate(:page => params[:page], :per_page => 20)
   end
 
   def recent
-        @users = User.includes(:status).order("statuses.updated_at DESC").paginate(:page => params[:page], :per_page => 30)
+        @users = User.includes(:status).order("statuses.updated_at DESC").paginate(:page => params[:page], :per_page => 20)
   end
 
 end
